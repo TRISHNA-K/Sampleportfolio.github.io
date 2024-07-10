c48cf03135744fe26260a9b4877c001326126890
TRISHNA-K <156880960+TRISHNA-K@users.noreply.github.com>
2024-07-10 12:21:18 +0530

    Create header.svelte

---
+++ header.svelte
@@ -0,0 +1,55 @@
+<script>
+
+</script>
+
+<header class="main-header">
+        <div class="logo">
+          <img src="logo.svg" alt="logo" >
+        </div>
+        <div class="navigation">
+          <a href="/" class="navigation-item">Home</a>
+          <a href="/about-me" class="navigation-item">About</a>
+          <span class="navigation-item">Blog</span>
+          <span class="navigation-item">Service</span>
+          <a href="/resume" class="navigation-item">Resume</a>
+         
+        </div>
+   
+   </header>
+ 
+ 
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+.main-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 70px;
+    padding-bottom: 70px;
+}
+.logo{
+    padding-left:122px;
+}
+.navigation {
+    position: absolute; 
+    display: flex;
+    align-items: center;
+    right: 120px; 
+    /* padding-left: 20px; */
+}
+.navigation-item {
+    padding-right: 58px;
+    padding-left: 23px;
+    font-size: 24px;
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    color: rgba(0, 0, 0, 1);
+    text-decoration: none;
+}
+</style>
