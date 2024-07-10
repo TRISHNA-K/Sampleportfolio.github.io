a8ff9ba7842edcd4bd9430b7a960bf490309d440
TRISHNA-K <156880960+TRISHNA-K@users.noreply.github.com>
2024-07-10 12:22:53 +0530

    Create footer.svelte

---
+++ footer.svelte
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+<footer class="footer">
+    <div class="social-media-icons">
+      <img src="icon-1.svg" alt="icon-1">
+      <img src="icon-2.svg" alt="icon-2">
+      <img src="icon-3.svg" alt="icon-3">
+      <img src="icon-4.svg" alt="icon-4">
+    </div>
+   </footer>
+<style>
+    .footer{
+    display: flex;
+    position: relative;
+}
+.social-media-icons{
+    display: flex;
+    position: absolute;
+    right:8%;
+    gap:30px;
+    padding-top:50px;
+}
+
+</style>
