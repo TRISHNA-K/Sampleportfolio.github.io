2ccfbc038c530733f3a1766b697bf541dddec144
TRISHNA-K <156880960+TRISHNA-K@users.noreply.github.com>
2024-07-10 12:22:14 +0530

    Create introduction.svelte

---
+++ introduction.svelte
@@ -0,0 +1,162 @@
+<script>
+
+</script>
+<div class="main-content">
+    <div class="content-section">
+    <div class="heading">Passionate <br> <span style="color: rgb(5, 9, 130);">UI/UX Designer</span> 
+
+    </div>
+    <div class="heading-2">
+      Elevating User Experiences with Creative UI/UX Design.
+
+    </div>
+    <div class="paragraph">
+      I'm a freelance UX designer based in Toronto, 
+Canada. I've worked with startups and established companies.
+I'm passionate about my work.
+    </div>
+    
+      <button class="button">Hire Me</button>
+      <div class="shapes">
+        <div class="rectangle"></div>
+        <div class="rectangle"></div>
+        <div class="rectangle"></div>
+        <div class="rectangle"></div>
+        <div class="rectangle"></div>
+      </div>
+        
+           
+     
+  </div>
+  <div class="image-section">
+    <div class="image-1">
+      <img src="image-1.png" alt="image-1" class="image-1">
+
+    </div>
+    <div class="image-border"></div>
+    <div class="name-image">
+      Nova Fernandez
+    </div>
+    <div class="shapes-2">
+      <div class="rectangle"></div>
+      <div class="rectangle"></div>
+      <div class="rectangle"></div>
+      <div class="rectangle"></div>
+      <div class="rectangle"></div>
+    </div>
+  </div>
+   </div>
+
+<style>
+    .main-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 120px;
+    position: relative;
+    width: 100%; 
+}
+.content-section {
+    flex: 1; 
+    padding-right: 50px; 
+    position: relative;
+}
+.heading {
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 75px;
+}
+.heading-2 {
+    font-family: "Lato", sans-serif;
+    font-weight: 300;
+    font-style: normal;
+    font-size: 24px;
+}
+.paragraph {
+    width: 744px;
+    font-family: "Lato", sans-serif;
+    font-weight: 200;
+    font-style: normal;
+    font-size: 28px;
+    padding-top: 40px;
+    padding-bottom: 70px;
+}
+.button {
+    background-color: rgb(3, 8, 123);
+    color: white;
+    width: 223px;
+    height: 80px;
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 40px;
+    cursor: pointer;
+   
+    
+}
+.rectangle{
+    width: 55px;
+    height: 40px;
+    background-color: rgb(3, 8, 123);
+    transform: rotate(90deg);
+    box-shadow: 0px 10px 20px 0px rgb(0, 0, 0,0.4);
+
+}
+.shapes{
+    position: absolute;
+    display: flex;
+    gap:20px;
+    justify-content: center;
+    left: -16%;
+    bottom: -30%;
+    gap:35px;
+    
+}
+
+.image-section{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+}
+.name-image{
+    font-size: 24px;
+    font-family: "Lato", sans-serif;
+  font-weight: 300;
+  font-style: normal;
+  padding-top:30px;
+}
+
+
+.image-1 {
+    display: flex;
+    position: relative;
+    width: 350px; 
+    height: 520px;
+    overflow: hidden; 
+   
+}
+
+.image-border {
+    position: absolute;
+    top: -22px;
+    left: 0;
+    width: 100%;
+    height: 93%;
+    border: 35px solid rgb(19, 26, 196);
+    border-radius: 239.5px;
+    box-sizing: border-box;
+}
+.shapes-2{
+display: flex;
+position: absolute;
+right: -34%;
+  gap:55px;
+  flex-direction: column;
+  top:10%;
+
+
+
+}
+</style>
